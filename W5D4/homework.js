7649c69bdd3bfce4f2c142b1fbc17b12506230c4
ajgosling <ajgosling2@gmail.com>
2018-09-19 23:17:00 -0700

    Start basic algorithms in JavaScript

---
+++ W5D4/homework.js
@@ -0,0 +1,64 @@
+function mysteryScoping1() {
+  var x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping1();
+
+function mysteryScoping2() {
+  const x = 'out of block';
+  if (true) {
+    const x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping2();
+
+function mysteryScoping3() {
+  const x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping3();
+
+function mysteryScoping4() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+
+mysteryScoping4();
+
+
+function madlib(verb, adjective, noun) {
+  return `We shall ${verb} the ${adjective} ${noun}.`
+}
+
+
+function isSubstring(phrase, subphrase) {
+  return phrase.includes(subphrase);
+}
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
